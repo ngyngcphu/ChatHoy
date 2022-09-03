3b6e49d83b799cc2c68bbfaed8e636201318fc92
ngyngcphu <nguyenngocphu225@gmail.com>
2022-09-03 20:24:33 +0700

    03/09

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -3,24 +3,34 @@ const { Server } = require("socket.io");
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "https://ngyngcphu.github.io",
+        origin: "http://127.0.0.1:5500",
         methods: ["GET", "POST"],
     },
 });
-httpServer.listen(process.env.PORT || 3000);
+httpServer.listen(3000);
+let numClients = [];
+let roomNumber = new Map();
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
     socket.on("joinRoom", (data) => {
         socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+        roomNumber.set(socket.id, data);
+        if (numClients[data] == undefined) {
+            numClients[data] = 1;
+        } else {
+            numClients[data]++;
+        }
+        io.to(data).emit("numberClients", numClients[data]);
     });
-
+    socket.on("back", (data) => {
+        numClients[data]--;
+    })
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
     });
 
     socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
+        let cnt = roomNumber.get(socket.id);
+        numClients[cnt]--;
+        socket.to(cnt).emit("numberClients", numClients[cnt]);
     });
 });
